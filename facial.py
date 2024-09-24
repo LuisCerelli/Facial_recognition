729496d5f2120495d7a48a438015353bbb9a4223
LuisCerelli <cerluisalberto@gmail.com>
2024-09-24 13:32:27 +0200

    update

--- facial.py
+++ facial.py
@@ -2,15 +2,15 @@ import cv2
 import os
 import imutils
 
-personName = 'Andrea'
-dataPath = '/Users/luisalbertocerelli/Desktop/00-Todo/03_Python_practicas/08_Reconocimiento_Facial/Data'
+personName = 'Luis'
+dataPath = '/Users/luisalbertocerelli/Desktop/00-Todo/03_Python_practicas/08_facial_recognition/data'
 personPath = dataPath + '/' + personName
 #print(personPath)
 if not os.path.exists(personPath):
     print('Carpeta creada: ',personPath)
     os.makedirs(personPath)
 
-cap = cv2.VideoCapture('Andrea.MOV')
+cap = cv2.VideoCapture('Luis.MOV')
 
 faceClassif = cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.xml')
 count = 0
@@ -21,7 +21,20 @@ while True:
     frame = imutils.resize(frame, width=640)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     auxFrame = frame.copy()
+ 
+    faces = faceClassif.detectMultiScale(gray,1.3,5)
+
+    for(x,y,w,h) in faces:
+        cv2.rectangle(frame, (x,y),(x+w,y+h),(0,255,0),2)
+        rostro = auxFrame[y:y+h,x:x+w]
+        rostro = cv2.resize(rostro,(150,150),interpolation = cv2.INTER_CUBIC)
+        cv2.imwrite(personPath + '/rostro_{}.jpg'.format(count),rostro)
+        count = count + 1
+    cv2.imshow('frame',frame) 
 
     k = cv2.waitKey(1)
     if k == 27 or count >=300:
         break
+
+cap.release()
+cv2.destroyAllWindows()
